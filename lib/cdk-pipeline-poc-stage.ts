7be123ae6f0396ac2327585104b0876b02c7c618
nsalbarde <nsalbarde@matson.com>
2024-05-23 10:27:36 -0700

    fixed the stage creation

--- lib/cdk-pipeline-poc-stage.ts
+++ lib/cdk-pipeline-poc-stage.ts
@@ -1,11 +1,11 @@
-import { CdkPipilinePocStack } from './cdk-pipiline-poc-stack';
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CdkAppStack } from './cdk-app-stack';
 
 export class CDKPipelinePocStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
-        new CdkPipilinePocStack(this, 'WebService');
+        new CdkAppStack(this, 'WebService');
     }
 }
