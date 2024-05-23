b1488f325079b4db49d8bfef6fc04e5f3d10673c
nsalbarde <nsalbarde@matson.com>
2024-05-22 22:09:30 -0700

    connected code repo with codepipiline using codestar

--- lib/cdk-pipiline-poc-stack.ts
+++ lib/cdk-pipiline-poc-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
-import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 import { Construct } from 'constructs';
 
 export class CdkPipilinePocStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Creates a CodeCommit repository called 'CdkPipilinePocRepo'
-        new codecommit.Repository(this, 'CdkPipilinePocRepo', {
-        repositoryName: "cdk-pipeline-poc"
-        });
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+        const cdkpipeline = new CodePipeline(this, "CdkPipeline", {
+            pipelineName: 'CdkPipelinePOC',
+            synth: new CodeBuildStep("SynthStep", {
+              input: CodePipelineSource.connection(
+                'nsalbarde/cdk-pipeline-poc',
+                "main",
+                {
+                  connectionArn: "arn:aws:codestar-connections:us-west-2:275416279984:connection/a96e8694-d581-49b7-a402-7eb4aa97fe00"
+                }
+              ),
+              commands: ["npm ci", "npm run build", "npx cdk synth"]
+            })
+          });
 
-        // Pipeline code goes here
     }
 }
