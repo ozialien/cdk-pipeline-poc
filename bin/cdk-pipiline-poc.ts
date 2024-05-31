76574de6cf5b8fae3f66b969ca4162729ab55ea9
nsalbarde <nsalbarde@matson.com>
2024-05-30 19:13:47 -0700

    Removed this line as it was causing build to fail for creating new IAM role

--- bin/cdk-pipiline-poc.ts
+++ bin/cdk-pipiline-poc.ts
@@ -4,9 +4,5 @@ import { CdkPipilinePocStack } from '../lib/cdk-pipiline-poc-stack';
 
 const app = new cdk.App();
 new CdkPipilinePocStack(app, 'CdkPipilinePocStack', {
-    env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION
-    },
     codeStarId: "a96e8694-d581-49b7-a402-7eb4aa97fe00",
 });
