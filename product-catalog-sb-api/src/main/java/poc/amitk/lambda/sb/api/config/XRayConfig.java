47ff1d5e8c4af95cb84742b74989f7f70c078472
ozialien <ozialien@gmail.com>
2024-10-30 10:52:07 -0700

    Added XRay Config

---
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/config/XRayConfig.java
@@ -0,0 +1,20 @@
+package poc.amitk.lambda.sb.api.config;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@XRayEnabled  // Enables X-Ray tracing across beans in this configuration
+@ConditionalOnProperty(name = "tracing.enabled", havingValue = "true") 
+public class XRayConfig {
+
+    @Bean
+    public AWSXRayRecorder awsXRayRecorder() {
+        return AWSXRay.getGlobalRecorder();  // Use the global recorder for tracing
+    }
+}
