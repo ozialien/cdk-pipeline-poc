ebc939a5a45192a39940339ad06480827f560fc0
ozialien <ozialien@gmail.com>
2024-11-04 08:11:45 -0700

    added

--- lib/sb-lambda-app-stack.ts
+++ lib/sb-lambda-app-stack.ts
@@ -7,9 +7,9 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as secretMgr from 'aws-cdk-lib/aws-secretsmanager';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { ExtendedProps } from './config';
 import { MatsonStack } from './common';
-
 
 export class SpringbootApiLambdaStack extends MatsonStack {
 
@@ -19,103 +19,87 @@ export class SpringbootApiLambdaStack extends MatsonStack {
     constructor(scope: Construct, id: string, props?: ExtendedProps) {
         super(scope, id, props);
 
-
-        //getting vpc for lambda
-        const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
-            vpcId: 'vpc-42de9927'
-        });
-
-        //getting security group for lambda
+        // Getting VPC, Security Group, Subnet, and Secret configurations
+        const vpc = ec2.Vpc.fromLookup(this, 'VPC', { vpcId: 'vpc-42de9927' });
         const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "SG", "sg-0ea85090b812c3265");
-
-        //getting subnet for lambda  
         const subnet = ec2.PrivateSubnet.fromSubnetAttributes(this, "subnet", { subnetId: "subnet-c56802b2" });
-
-        //getting secret from secret manager
         const dbAccessSecretId = ssm.StringParameter.valueForStringParameter(this, '/cdkpipelinepoc/matlab/dbaccesssecretid');
         const secretPartialArn = `arn:aws:secretsmanager:${this.region}:${this.account}:secret:${dbAccessSecretId}`;
         const dbAccessSecret = secretMgr.Secret.fromSecretPartialArn(this, 'SecretFromCompleteArn', secretPartialArn);
+
+        // Lambda configuration
         if (props?.extra?.lambda) {
-            const lambdaRuntime = props?.extra?.lambda?.java?.version ?  props?.extra?.lambda?.java?.version : lambda.Runtime.JAVA_21;            
-            let lambdaInformation: cdk.aws_lambda.FunctionProps = {
-                functionName: props?.extra?.lambda.name,
-                runtime: lambdaRuntime ,
-                code: props?.extra?.lambda?.code,
-                memorySize: props?.extra?.lambda.memory,
-                handler: props?.extra?.lambda.handler,
+            const lambdaInformation: lambda.FunctionProps = {
+                functionName: props.extra.lambda.name,
+                runtime: props.extra.lambda.java?.version ?? lambda.Runtime.JAVA_21,
+                code: props.extra.lambda.code,
+                memorySize: props.extra.lambda.memory,
+                handler: props.extra.lambda.handler,
                 snapStart: SnapStartConf.ON_PUBLISHED_VERSIONS,
-                vpc: vpc,
+                vpc,
                 vpcSubnets: { subnets: [subnet] },
                 securityGroups: [securityGroup],
                 environment: {
                     "datasource_secret_id": dbAccessSecretId,
                 },
-                timeout: props?.extra?.cdk?.timeout ? cdk.Duration.seconds(props?.extra?.cdk?.timeout) : cdk.Duration.seconds(30)
+                timeout: cdk.Duration.seconds(props.extra.cdk?.timeout ?? 30),
+                tracing: props.extra.lambda.xrayEnabled ? lambda.Tracing.ACTIVE : lambda.Tracing.DISABLED,
             };
 
-            if (props?.extra?.lambda.xrayEnabled) {
-                Object.assign(lambdaInformation, { tracing: lambda.Tracing.ACTIVE });
-            } else {
-                Object.assign(lambdaInformation, { tracing: lambda.Tracing.DISABLED });
-            }
-            //Setup Lambda Function
-            const springBootApiLambdaCdkPoc = new Function(this,  props?.extra?.lambda.id, lambdaInformation);
+            const springBootApiLambdaCdkPoc = new Function(this, props.extra.lambda.id, lambdaInformation);
 
+            // X-Ray permissions
+            springBootApiLambdaCdkPoc.role?.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess'));
 
-            // Grant X-Ray permissions to Lambda
-            springBootApiLambdaCdkPoc.role?.addManagedPolicy(
-                ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess')
-            );
-
-            // const version = new Version(this, 'ProductCatalogLambdaVersion', {
-            //     lambda: springBootApiLambdaCdkPoc,
-            //   }); 
-            // const version = springBootApiLambdaCdkPoc.currentVersion;
+            // Lambda alias
             springBootApiLambdaCdkPoc.addAlias("Live");
 
-
-            //grant function to read secret
+            // Secret read permissions
             dbAccessSecret.grantRead(springBootApiLambdaCdkPoc);
 
+            // Cognito User Pool and App Client
+            const userPool = new cognito.UserPool(this, 'UserPool', {
+                userPoolName: 'MyUserPool',
+                signInAliases: { email: true },
+            });
+            const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+                userPool,
+                generateSecret: true,
+                oAuth: {
+                    flows: { authorizationCodeGrant: true },
+                    scopes: [cognito.OAuthScope.OPENID, cognito.OAuthScope.PROFILE],
+                    callbackUrls: ['https://yourcallbackurl.com'],
+                },
+            });
+
+            // API Gateway configuration
             let apiInformation: apigateway.LambdaRestApiProps = {
                 handler: springBootApiLambdaCdkPoc,
                 proxy: false,
-
+                deployOptions: { tracingEnabled: props.extra.lambda.xrayEnabled ?? false },
             };
-            if (props?.extra?.oas) {
-                Object.assign(apiInformation, {
-                    apiDefinition: apigateway.ApiDefinition.fromAsset(props?.extra?.oas),
-                });
+            if (props.extra.oas) {
+                apiInformation.apiDefinition = apigateway.ApiDefinition.fromAsset(props.extra.oas);
             }
-            if (props?.extra?.lambda.xrayEnabled) {
-                Object.assign(apiInformation, {
-                    deployOptions: {
-                        tracingEnabled: true, // Enable X-Ray tracing for API Gateway
-                    }
-                });
-            } else {
-                Object.assign(apiInformation, {
-                    deployOptions: {
-                        tracingEnabled: false, // Disable X-Ray tracing for API Gateway
-                    }
-                });
-            }
-            // Define the API Gateway resource
-            const api = new apigateway.LambdaRestApi(this, props?.extra?.apiGateway?.name ? props?.extra?.apiGateway?.name : '', apiInformation);
-            this.apiEndpointUrl = new cdk.CfnOutput(this, "ApiEndpointUrl", {
-                value: api.url,
-            });
-            this.lambdaFunctionName = new cdk.CfnOutput(this, 'lambdaFunctionName', { value: lambdaInformation.functionName ? lambdaInformation.functionName : '' });
+            const api = new apigateway.LambdaRestApi(this, props.extra.apiGateway?.name ?? '', apiInformation);
 
-            // Define the '/products' resource with a GET method
+            this.apiEndpointUrl = new cdk.CfnOutput(this, "ApiEndpointUrl", { value: api.url });
+            this.lambdaFunctionName = new cdk.CfnOutput(this, 'lambdaFunctionName', { value: lambdaInformation.functionName ?? '' });
+
+            // Cognito Authorizer
+            const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'CognitoAuthorizer', {
+                cognitoUserPools: [userPool],
+            });
+
+            // Define the '/products' resource with secured methods
             const products = api.root.addResource('products');
-            products.addMethod('GET'); //gets all the products
-            products.addMethod('POST'); //add new product
-            products.addMethod('DELETE'); //delete all products
+            products.addMethod('GET', undefined, { authorizationType: apigateway.AuthorizationType.COGNITO, authorizer });
+            products.addMethod('POST', undefined, { authorizationType: apigateway.AuthorizationType.COGNITO, authorizer });
+            products.addMethod('DELETE', undefined, { authorizationType: apigateway.AuthorizationType.COGNITO, authorizer });
 
             const product = products.addResource("{productSku}");
-            product.addMethod('GET'); //get a specific product
-            product.addMethod('DELETE'); //delete a specific product
+            product.addMethod('GET', undefined, { authorizationType: apigateway.AuthorizationType.COGNITO, authorizer });
+            product.addMethod('DELETE', undefined, { authorizationType: apigateway.AuthorizationType.COGNITO, authorizer });
         } else {
             throw new Error("Missing Lambda configuration!");
         }
