5643b39bd0155b5f79653f9ce36cf43a834953ca
ozialien <ozialien@gmail.com>
2024-11-18 13:51:58 -0700

    simplifying xray segment enablement

--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
@@ -1,25 +1,15 @@
 package poc.amitk.lambda.sb.api.infra;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Segment;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import poc.amitk.lambda.sb.api.ProductCatalogSbApiApplication;
 
 /**
@@ -27,102 +17,27 @@ import poc.amitk.lambda.sb.api.ProductCatalogSbApiApplication;
  */
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    // Create a logger instance
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
 
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(ProductCatalogSbApiApplication.class);
-            // If you are using HTTP APIs with the version 2.0 of the proxy model, use the
-            // getHttpApiV2ProxyHandler
-            // method: handler =
-            // SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
 
-    /**
-     * 
-     * Being able to dynamically switch on and off XRay tracing via the api gateway
-     * trace header
-     * is apparently a cost save.
-     * Even when XRay tracing is switched on in Lambda you can ihitiate a full trace
-     * this way.
-     * 
-     * Recorded traces: $5 per million traces recorded
-     * Retrieved traces: $0.50 per million traces retrieved
-     * Scanned traces: $0.50 per million traces scanned
-     * X-Ray Insights traces stored: $1 per million traces recorded
-     * Sampling rate: The chosen sampling rate is multiplied by the request or API
-     * call rate to estimate costs
-     * 
-     * 
-     * @param inputStream
-     * @param outputStream
-     * @param context
-     * @throws IOException
-     */
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream,
             Context context)
             throws IOException {
         String methodName = new Exception().getStackTrace()[0].getMethodName();
         logger.info("Entering {}.{}", this.getClass().getName(), methodName);
-
-        ByteArrayOutputStream cachedStream = new ByteArrayOutputStream();
-        inputStream.transferTo(cachedStream);
-        byte[] inputBytes = cachedStream.toByteArray();
-        InputStream cachedInputForHeaders = new ByteArrayInputStream(inputBytes);
-        InputStream cachedInputForHandler = new ByteArrayInputStream(inputBytes);
-
-        logger.info("Received JSON data: {}", new String(inputBytes,
-                StandardCharsets.UTF_8));
-
-        AwsProxyRequest request = objectMapper.readValue(cachedInputForHeaders,
-                AwsProxyRequest.class);
-        String traceHeader = request.getHeaders().getOrDefault("X-Amzn-Trace-Id",
-                null);
-
-        logger.info("X-Amzn-Trace-Id: {}", traceHeader);
-        Segment segment = null;
-
-        if (traceHeader != null) {
-            // If tracing header is present, start a new X-Ray segment for this request
-            segment = AWSXRay.beginSegment("ProductCatalogService");
-        }
-
         try {
-            // Forward the request to the handler
-            handler.proxyStream(cachedInputForHandler, outputStream, context);
-        } catch (Exception e) {
-            if (segment != null) {
-                segment.addException(e);
-            }
-            throw e;
+            handler.proxyStream(inputStream, outputStream, context);
         } finally {
-            // End the segment if it was started
-            if (segment != null) {
-                AWSXRay.endSegment();
-            }
             logger.info("Exiting {}.{}", this.getClass().getName(), methodName);
-
         }
     }
-
-    // @Override
-    // public void handleRequest(InputStream inputStream, OutputStream outputStream,
-    // Context context)
-    // throws IOException {
-    // String methodName = new Exception().getStackTrace()[0].getMethodName();
-    // logger.info("Entering {}.{}", this.getClass().getName(), methodName);
-    // try {
-    // handler.proxyStream(inputStream, outputStream, context);
-    // } finally {
-    // logger.info("Exiting {}.{}", this.getClass().getName(), methodName);
-    // }
-    // }
 }
--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/XRayConfig.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/XRayConfig.java
@@ -1,20 +1,30 @@
-    package poc.amitk.lambda.sb.api.infra;
+package poc.amitk.lambda.sb.api.infra;
 
-    import com.amazonaws.xray.AWSXRay;
-    import com.amazonaws.xray.AWSXRayRecorder;
-    import com.amazonaws.xray.spring.aop.XRayEnabled;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 
-    import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
+import poc.amitk.lambda.sb.api.ProductCatalogSbApiApplication;
 
-    @Configuration
-    @XRayEnabled  // Enables X-Ray tracing across beans in this configuration
-    @ConditionalOnProperty(name = "xray.tracing.enabled", havingValue = "true") 
-    public class XRayConfig {
+import javax.servlet.Filter;
 
-        @Bean
-        public AWSXRayRecorder awsXRayRecorder() {
-            return AWSXRay.getGlobalRecorder();  // Use the global recorder for tracing
-        }
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@XRayEnabled // Enables X-Ray tracing across beans in this configuration
+@ConditionalOnProperty(name = "xray.tracing.enabled", havingValue = "true")
+public class XRayConfig {
+/* 
+    @Bean
+    public AWSXRayRecorder awsXRayRecorder() {
+        return AWSXRay.getGlobalRecorder(); // Use the global recorder for tracing
     }
+*/
+    @Bean
+    public Filter TracingFilter() {
+        return new AWSXRayServletFilter(ProductCatalogSbApiApplication.class.getName());
+    }
+}
