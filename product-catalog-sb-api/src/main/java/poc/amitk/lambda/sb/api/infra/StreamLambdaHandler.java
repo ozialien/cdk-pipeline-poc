43514fcb15e7d0a89a4a67303d927b4e7d74206e
ozialien <ozialien@gmail.com>
2024-11-19 11:06:19 -0700

    added

--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
@@ -3,10 +3,12 @@ package poc.amitk.lambda.sb.api.infra;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import org.slf4j.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
@@ -25,6 +27,8 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     private static final String CURRENT_CLASS_NAME = StreamLambdaHandler.class.getName();
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+    // Obtain a Tracer instance
+    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("ProductCatalogService");
 
     static {
         try {
@@ -44,18 +48,20 @@ public class StreamLambdaHandler implements RequestStreamHandler {
         String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         logger.info("Entering {}.{}", CURRENT_CLASS_NAME, methodName);
 
-        // Obtain a Tracer instance
-        Tracer tracer = GlobalOpenTelemetry.getTracer("ProductCatalogService");
-
         // Start a new Span representing the handleRequest method
         Span span = tracer.spanBuilder(methodName).startSpan();
 
         // Make the span the current span
         try (Scope scope = span.makeCurrent()) {
-            // Optionally, set attributes on the span
-            span.setAttribute("class.name", CURRENT_CLASS_NAME);
-            span.setAttribute("method.name", methodName);
+            String traceid = span.getSpanContext().getTraceId();
+            SpanContext spanContext = span.getSpanContext();
+            MDC.put("traceId", spanContext.getTraceId());
+            MDC.put("spanId", spanContext.getSpanId());
 
+            // Optionally, set attributes on the span
+            span.setAttribute("aws.lambda.function_name", context.getFunctionName());
+            span.setAttribute("aws.lambda.request_id", context.getAwsRequestId());
+            logger.info("TraceID: {}", traceid);
             // Call the handler method
             handler.proxyStream(inputStream, outputStream, context);
 
@@ -63,6 +69,7 @@ public class StreamLambdaHandler implements RequestStreamHandler {
             // Record the exception in the span
             span.recordException(e);
             span.setStatus(StatusCode.ERROR, "Exception in handleRequest");
+            logger.error("Error occurred", e);
             // Re-throw the exception to maintain the original behavior
             throw e;
         } finally {
