c264458190cceb719501202252ba02fb72fcec14
ozialien <ozialien@gmail.com>
2024-11-26 15:27:51 -0700

    updated

---
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
@@ -0,0 +1,27 @@
+package poc.amitk.lambda.sb.api.infra;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "aws.xray.enabled", havingValue = "true")
+public class AwsXRayConfig {
+    // Create a logger instance
+    private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Setting up XRay Tracing");
+
+        // Initialize the AWS X-Ray recorder with default settings
+        AWSXRay.setGlobalRecorder(AWSXRayRecorderBuilder.standard().build());
+
+        logger.info("Setting up XRay Tracing Done");
+    }
+}
