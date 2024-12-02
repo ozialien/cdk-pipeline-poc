24b1f0aa11e61bee48816819336c0519a7700091
ozialien <ozialien@gmail.com>
2024-12-02 12:17:22 -0700

    placed in filter

--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
@@ -1,20 +1,27 @@
 package poc.amitk.lambda.sb.api.infra;
 
 import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
-import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.entities.Segment;
+import com.amazonaws.xray.jakarta.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.strategy.jakarta.SegmentNamingStrategy;
 import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
 
+import io.micrometer.common.lang.Nullable;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
+import java.io.IOException;
 import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//
-//
-//import javax.servlet.Filter;
-
+import org.slf4j.MDC;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -27,14 +34,67 @@ public class AwsXRayConfig {
     // Create a logger instance
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
 
+    public class CustomXRayServletFilter extends AWSXRayServletFilter {
+        private static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
+        private static final String TRACE_ID_HEADER = "X-Amzn-Trace-Id";
+        private static final String CORRELATION_ID_MDC_KEY = "correlationId";
+        private static final String TRACE_ID_MDC_KEY = "traceId";
+
+        public CustomXRayServletFilter() {
+            super((SegmentNamingStrategy) null);
+        }
+
+        public CustomXRayServletFilter(String fixedSegmentName) {
+            super(SegmentNamingStrategy.fixed(fixedSegmentName));
+        }
+
+        public CustomXRayServletFilter(@Nullable SegmentNamingStrategy segmentNamingStrategy) {
+            super(segmentNamingStrategy, null);
+        }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("CustomXRayServletFilter is beginning to process request: " + request.toString());
+            }
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            try {
+                // Retrieve the X-Correlation-ID header
+                String correlationId = httpRequest.getHeader(CORRELATION_ID_HEADER);
+                // Retrieve the X-Amzn-Trace-Id header
+                String amznTraceId = httpRequest.getHeader(TRACE_ID_HEADER);
+                // Optionally, add these IDs to the logging context
+                if (correlationId != null && !correlationId.isEmpty()) {
+                    MDC.put(CORRELATION_ID_MDC_KEY, correlationId);
+                }
+                if (amznTraceId != null && !amznTraceId.isEmpty()) {
+                    MDC.put(TRACE_ID_MDC_KEY, amznTraceId);
+                }
+                super.doFilter(request, response, chain);
+            } finally {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("CustomXRayServletFilter is finished processing request: " + request.toString());
+                }
+                // Clean up the MDC to prevent data leakage between threads
+                MDC.remove(CORRELATION_ID_MDC_KEY);
+                MDC.remove(TRACE_ID_MDC_KEY);
+            }
+        }
+
+    }
+
     ////
     //
-    // Thits posed problems given that ja vax.servlet.Filter isn't provided for SpringBoot
+    // Thits posed problems given that javax.servlet.Filter isn't provided for
+    //// SpringBoot
     // 3.x services.
     //
     @Bean
-    public AWSXRayServletFilter TracingFilter() {
-        return new AWSXRayServletFilter("ProductCatalogService");
+    public CustomXRayServletFilter TracingFilter() {
+        CustomXRayServletFilter xrayFilter = new AwsXRayConfig.CustomXRayServletFilter("ProductCatalogService");
+        return xrayFilter;
     }
 
     @PostConstruct
--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/StreamLambdaHandler.java
@@ -16,11 +16,6 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Segment;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import poc.amitk.lambda.sb.api.ProductCatalogSbApiApplication;
 
 /**
@@ -28,84 +23,33 @@ import poc.amitk.lambda.sb.api.ProductCatalogSbApiApplication;
  */
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    private static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
-    private static final String TRACE_ID_HEADER = "X-Amzn-Trace-Id";
-    private static final String CORRELATION_ID_MDC_KEY = "correlationId";
-    private static final String TRACE_ID_MDC_KEY = "traceId";
+   
     private static final String CURRENT_CLASS_NAME = StreamLambdaHandler.class.getName();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
+    
     // Create a logger instance
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
 
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(ProductCatalogSbApiApplication.class);
-            // If you are using HTTP APIs with the version 2.0 of the proxy model, use the
-            // getHttpApiV2ProxyHandler
-            // method: handler =
-            // SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
+
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream,
             Context context)
             throws IOException {
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        inputStream.transferTo(byteArrayOutputStream);
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        InputStream inputStreamCopy1 = new ByteArrayInputStream(bytes);
-        InputStream inputStreamCopy2 = new ByteArrayInputStream(bytes);
-
-        String correlationId = null;
-        String traceId = null;
-        JsonNode eventNode = objectMapper.readTree(inputStreamCopy1);
-        JsonNode headersNode = eventNode.get("headers");
-
-        if (headersNode != null && headersNode.has(CORRELATION_ID_HEADER)) {
-            correlationId = headersNode.get(CORRELATION_ID_HEADER).asText();
-        }
-        if (correlationId == null) {
-            correlationId = context.getAwsRequestId();
-        }
-        MDC.put(CORRELATION_ID_MDC_KEY, correlationId);
-
-        Segment segment = null;
-        if (headersNode != null && headersNode.has(TRACE_ID_HEADER)) {
-            traceId = headersNode.get(TRACE_ID_HEADER).asText();
-            logger.info("Fetched traceId from API Gateway Proxy Header: {}", traceId);
-            segment = AWSXRay.beginSegment("ProductCatalogService");
-
-        }
-
-        if (traceId != null) {
-            MDC.put(TRACE_ID_MDC_KEY, traceId);
-        } else {
-            MDC.put(TRACE_ID_MDC_KEY, "no-trace");    
-        }
-
         String methodName = new Exception().getStackTrace()[0].getMethodName();
-
         try {
             logger.info("Entering {}.{}", CURRENT_CLASS_NAME, methodName);
-            handler.proxyStream(inputStreamCopy2, outputStream, context);
-        } catch (Exception e) {
-            if (segment != null) {
-                segment.addException(e);
-            }
-            throw e;
+            handler.proxyStream(inputStream, outputStream, context);        
         } finally {
-            if (segment != null) {
-                AWSXRay.endSegment();
-            }
             logger.info("Exiting {}.{}", CURRENT_CLASS_NAME, methodName);
-            MDC.remove(CORRELATION_ID_MDC_KEY);
         }
     }
 }
