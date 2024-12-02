f2d7528965d2b2b57921cf8d1fca50ec7fe18a2e
Ernest Rider <erider@matson.com>
2024-12-02 08:24:53 -0800

    added

--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
@@ -15,13 +15,30 @@ public class AwsXRayConfig {
     // Create a logger instance
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
 
+
+    static {
+     
+      }
+
+
+
+
     @PostConstruct
     public void init() {
         logger.info("Setting up XRay Tracing");
 
-        // Initialize the AWS X-Ray recorder with default settings
-        AWSXRay.setGlobalRecorder(AWSXRayRecorderBuilder.standard().build());
-
-        logger.info("Setting up XRay Tracing Done");
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
+        URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
+        builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+        AWSXRay.setGlobalRecorder(builder.build());
+        AWSXRay.beginSegment("Scorekeep-init");
+        if ( System.getenv("NOTIFICATION_EMAIL") != null ){
+          try { Sns.createSubscription(); }
+          catch (Exception e ) {
+            logger.warn("Failed to create subscription for email "+  System.getenv("NOTIFICATION_EMAIL"));
+          }
+        }
+        
+        AWSXRay.endSegment();     logger.info("Setting up XRay Tracing Done");
     }
 }
