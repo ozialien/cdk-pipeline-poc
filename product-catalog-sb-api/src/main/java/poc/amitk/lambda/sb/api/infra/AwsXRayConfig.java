dcbb7efad45d2f89a666d2457d3751ddde26460a
ozialien <ozialien@gmail.com>
2024-12-02 11:03:49 -0700

    xray 4th rendition

--- product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
+++ product-catalog-sb-api/src/main/java/poc/amitk/lambda/sb/api/infra/AwsXRayConfig.java
@@ -2,6 +2,7 @@ package poc.amitk.lambda.sb.api.infra;
 
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
 
 import jakarta.annotation.PostConstruct;
@@ -10,8 +11,13 @@ import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//
+//
+//import javax.servlet.Filter;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -20,6 +26,16 @@ import org.springframework.context.annotation.Configuration;
 public class AwsXRayConfig {
     // Create a logger instance
     private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+
+    ////
+    //
+    // Thits posed problems given that ja vax.servlet.Filter isn't provided for SpringBoot
+    // 3.x services.
+    //
+    @Bean
+    public AWSXRayServletFilter TracingFilter() {
+        return new AWSXRayServletFilter("ProductCatalogService");
+    }
 
     @PostConstruct
     public void init() {
@@ -30,6 +46,6 @@ public class AwsXRayConfig {
         AWSXRay.setGlobalRecorder(builder.build());
         AWSXRay.beginSegment("setup-xray");
         logger.info("Setting up XRay Tracing Done");
-        AWSXRay.endSegment();   
+        AWSXRay.endSegment();
     }
 }
